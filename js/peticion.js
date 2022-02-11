dc0e67fcc2f65bead84e89b6972ee7772bfb2496
Richardeveloper <richardjimenez.9641@outlook.com>
2022-02-11 01:46:23 -0500

    Style Form

--- js/peticion.js
+++ js/peticion.js
@@ -1,13 +1,13 @@
-function getData() { 
-      fetch('http://127.0.0.1:8000/api/images', {
-  method: 'GET',
-  headers:{
-    'Content-Type': 'application/json'
-  }
-})
-            .then(res => res.json())
-            .then(data => {
-                  console.log(data);
-            });
-}
-getData()
+// function getData() { 
+//       fetch('http://127.0.0.1:8000/api/images', {
+//   method: 'GET',
+//   headers:{
+//     'Content-Type': 'application/json'
+//   }
+// })
+//             .then(res => res.json())
+//             .then(data => {
+//                   console.log(data);
+//             });
+// }
+// getData()
